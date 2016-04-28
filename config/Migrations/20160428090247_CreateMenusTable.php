6883be9ca9cec04394299140db81422a717db9b0
Antonis Flangofas <a.flangofas@qobo.biz>
2016-04-28 14:12:09 +0300

    Migrations (task #1907)

---
+++ config/Migrations/20160428090247_CreateMenusTable.php
@@ -0,0 +1,44 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateMenusTable extends AbstractMigration
+{
+    public $autoId = false;
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('menus');
+        $table->addColumn('id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('active', 'boolean', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'date', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'date', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addPrimaryKey([
+            'id',
+        ]);
+        $table->create();
+    }
+}
