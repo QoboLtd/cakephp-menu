ab157a13eafb7791e6282bbe75dec505fe4f675b
Antonis Flangofas <a.flangofas@qobo.biz>
2016-04-28 14:32:03 +0300

    Set layout (task #1907)

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,4 +7,15 @@ use App\Controller\AppController as BaseController;
 class AppController extends BaseController
 {
 
+    /**
+     * Initialization hook method.
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->viewBuilder()->layout('QoboAdminPanel.basic');
+    }
+
 }
