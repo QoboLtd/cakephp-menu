c27bffcd026804512edc800b48bd40a3620a0865
Antonis Flangofas <flangofas@users.noreply.github.com>
2016-05-09 17:04:36 +0300

    Removed forced layout (task #1957)

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,15 +6,4 @@ use App\Controller\AppController as BaseController;
 
 class AppController extends BaseController
 {
-
-    /**
-     * Initialization hook method.
-     *
-     * @return void
-     */
-    public function initialize()
-    {
-        parent::initialize();
-        $this->viewBuilder()->layout('QoboAdminPanel.basic');
-    }
 }
