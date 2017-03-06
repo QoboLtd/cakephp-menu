e721a27957182fa3213334800b18c22c7b4a36d6
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-03-06 14:30:12 +0200

    Removed obsolete _getDirControllers() method (task #3205)

--- src/View/Helper/MenuHelper.php
+++ src/View/Helper/MenuHelper.php
@@ -37,34 +37,4 @@ class MenuHelper extends Helper
 
         return $menu;
     }
-
-    /**
-     * Method that retrieves controller names
-     * found on the provided directory path.
-     * @param  string $path   directory path
-     * @param  string $plugin plugin name
-     * @param  bool   $fqcn   flag for using fqcn
-     * @return array          controller names
-     */
-    protected function _getDirControllers($path, $plugin = null, $fqcn = true)
-    {
-        $controllers = [];
-        if (file_exists($path)) {
-            $dir = new \DirectoryIterator($path);
-            foreach ($dir as $fileinfo) {
-                $className = $fileinfo->getBasename('.php');
-                if ($fileinfo->isFile() && 'AppController' !== $className) {
-                    if (!empty($plugin)) {
-                        $className = $plugin . '.' . $className;
-                    }
-                    if ($fqcn === true) {
-                        $className = \Cake\Core\App::className($className, 'Controller');
-                    }
-                    $controllers[] = $className;
-                }
-            }
-        }
-
-        return $controllers;
-    }
 }
