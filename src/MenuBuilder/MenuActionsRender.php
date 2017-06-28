5dcec9d6cb19360640c5cf1e3f4e7d6030effafa
Michael Stepanov <michael@stepanoff.org>
2017-06-28 17:13:00 +0300

    Added new menu render for actions menu - task #809

---
+++ src/MenuBuilder/MenuActionsRender.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Menu\MenuBuilder;
+
+/**
+ *  MenuActionsRender class
+ *
+ *  rendering menu with buttons
+ */
+class MenuActionsRender extends BaseMenuRenderClass
+{
+    /**
+     *  __construct method
+     *
+     * @param Menu\MenuBuilder\Menu $menu menu collection
+     * @return void
+     */
+    public function __construct(Menu $menu, $viewEntity)
+    {
+        parent::__construct($menu, $viewEntity);
+        
+        $this->format = [
+            'menuStart' => '<div class="btn-group btn-group-xs">',
+            'menuEnd' => '</div>',
+            'childMenuStart' => '',
+            'childMenuEnd' => '',
+            'itemStart' => '',
+            'itemEnd' => '',
+            'item' => '<a href="%url%" title="%label%" class="btn btn-default" data-type="%dataType%" data-confirm-msg="%confirmMsg%"><i class="fa fa-%icon%"></i></a>',
+            'itemWithChildren' => '',
+        ];
+    }
+}
