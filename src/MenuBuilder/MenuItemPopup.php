fa0714969504c949785cc757f83c8594c7bf3743
Michael Stepanov <michael@stepanoff.org>
2017-07-18 17:54:19 +0300

    Added menu item class for link with popup - task #809

---
+++ src/MenuBuilder/MenuItemPopup.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Menu\MenuBuilder;
+
+class MenuItemPopup extends BaseMenuItem
+{
+    /**
+     * @var $dataType 
+     */
+    protected $dataType = '';
+
+    /**
+     *  getDataType method
+     *
+     * @return string dataType attribute
+     */
+    public function getDataType()
+    {
+        return $this->dataType;
+    }
+
+    /**
+     *  setDataType method
+     *
+     * @param string $dataType for menu item
+     * @return void
+     */
+    public function setDataType($dataType)
+    {
+        $this->dataType = $dataType;
+    }
+}
