0be55fffde9d442baaf1b008056bfcdb21d7a12c
Andrey Vystavkin <jujav4ik@gmail.com>
2017-07-20 13:28:12 +0300

    Added some basic setters unit tests (task #809)

--- src/MenuBuilder/BaseMenuItem.php
+++ src/MenuBuilder/BaseMenuItem.php
@@ -233,7 +233,7 @@ abstract class BaseMenuItem implements MenuItemInterface
      */
     public function setOrder($order)
     {
-        $this->order = $order;
+        $this->order = (int)$order;
     }
 
     /**
