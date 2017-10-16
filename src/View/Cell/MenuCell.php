ce5dd55577d16a0ea8e2e087d345aaad45255539
George Constantinou <georgec20001@gmail.com>
2017-10-16 10:06:01 +0300

    Remove unused logic from Menu cell (task #4603)

--- src/View/Cell/MenuCell.php
+++ src/View/Cell/MenuCell.php
@@ -29,21 +29,14 @@ class MenuCell extends Cell
      *
      * @var array
      */
-    protected $_user = [];
-
-    /**
-     * Menu rendering format.
-     *
-     * @var array
-     */
-    protected $_format = [];
+    protected $user = [];
 
     /**
      * Full-base URL flag.
      *
      * @var boolean
      */
-    protected $_fullBaseUrl = false;
+    protected $fullBaseUrl = false;
 
     /**
      * Menu item defaults.
@@ -61,63 +54,24 @@ class MenuCell extends Cell
     ];
 
     /**
-     * Supported render formats.
-     *
-     * @var array
-     */
-    protected $_renderFormats = [
-        RENDER_AS_PROVIDED => [
-            'menuStart' => '',
-            'menuEnd' => '',
-            'itemStart' => '',
-            'itemEnd' => '',
-            'item' => '%label%',
-        ],
-        RENDER_AS_LIST => [
-            'menuStart' => '<ul>',
-            'menuEnd' => '</ul>',
-            'itemStart' => '<li>',
-            'itemEnd' => '</li>',
-            'item' => '<a href="%url%"><i class="fa fa-%icon%"></i> %label%</a>',
-        ],
-        RENDER_AS_DROPDOWN => [
-            'menuStart' => '<select>',
-            'menuEnd' => '</select>',
-            'itemStart' => '<option>',
-            'itemEnd' => '</option>',
-            'item' => '<i class="fa fa-%icon%"></i> %label%',
-        ],
-        RENDER_AS_NONE => [
-            'menuStart' => '',
-            'menuEnd' => '',
-            'itemStart' => '',
-            'itemEnd' => '',
-            'item' => '',
-        ]
-    ];
-
-    /**
      * Default display method.
      *
      * Retrieves menu items and rendering format and passes them to the Cell View.
      *
      * @param string $name Menu name
-     * @param string|array $renderAs Rendering format name or data array
      * @param array $user User info
      * @param bool $fullBaseUrl Full-base URL flag
      * @return void
      */
-    public function display($name, $renderAs, array $user = [], $fullBaseUrl = false)
+    public function display($name, array $user, $fullBaseUrl = false)
     {
         // validate menu name
         $this->_validateName($name);
 
-        $this->_format = $this->_getFormat($renderAs);
-
         $this->loadModel('Menu.Menus');
 
-        $this->_user = !empty($user) ? $user : $this->_getUser($user);
-        $this->_fullBaseUrl = (bool)$fullBaseUrl;
+        $this->user = $user;
+        $this->fullBaseUrl = (bool)$fullBaseUrl;
 
         // get menu
         $menu = $this->Menus->findByName($name)->firstOrFail();
@@ -133,8 +87,7 @@ class MenuCell extends Cell
         }
 
         $this->set('menuItems', $menuItems);
-        $this->set('user', $this->_user);
-        $this->set('format', $this->_format);
+        $this->set('user', $this->user);
         $this->set('name', $name);
     }
 
@@ -149,8 +102,8 @@ class MenuCell extends Cell
     {
         $event = new Event((string)EventName::GET_MENU_ITEMS(), $this, [
             'name' => $menu->name,
-            'user' => $this->_user,
-            'fullBaseUrl' => $this->_fullBaseUrl,
+            'user' => $this->user,
+            'fullBaseUrl' => $this->fullBaseUrl,
             'modules' => $modules
         ]);
         $this->eventManager()->dispatch($event);
@@ -257,56 +210,6 @@ class MenuCell extends Cell
         if (empty($name)) {
             throw new InvalidArgumentException('Menu [name] cannot be empty.');
         }
-    }
-
-    /**
-     * Current session user getter method.
-     *
-     * Uses PHP's $_SESSION global variable to fetch current session user.
-     *
-     * @return array
-     */
-    protected function _getUser()
-    {
-        if (isset($_SESSION['Auth']['User'])) {
-            return $_SESSION['Auth']['User'];
-        };
-
-        return [];
-    }
-
-    /**
-     * Menu rendering format getter.
-     *
-     * @param string|array $renderAs Rendering format name or data array
-     * @return array
-     * @throws \InvalidArgumentException
-     */
-    protected function _getFormat($renderAs)
-    {
-        $defaults = [
-            'menuStart' => '<ul>',
-            'menuEnd' => '</ul>',
-            'childMenuStart' => '<ul>',
-            'childMenuEnd' => '</ul>',
-            'itemStart' => '<li>',
-            'itemEnd' => '</li>',
-            'item' => '%label%',
-        ];
-
-        if (is_string($renderAs)) {
-            if (!array_key_exists($renderAs, $this->_renderFormats)) {
-                throw new InvalidArgumentException('Render format [' . $renderAs . '] not found.');
-            }
-
-            return $renderFormats[$renderAs];
-        }
-
-        if (is_array($renderAs)) {
-            return array_merge($defaults, $renderAs);
-        }
-
-        throw new InvalidArgumentException('[renderAs] variable must be an array or string.');
     }
 
     /**
