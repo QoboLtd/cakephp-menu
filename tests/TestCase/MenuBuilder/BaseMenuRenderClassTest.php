5f0bcb832e6afb1b93e5f21da332535fe32f3dcf
George Constantinou <georgec20001@gmail.com>
2018-01-09 10:52:04 +0200

    Unit test for modal menu item (task #5156)

--- tests/TestCase/MenuBuilder/BaseMenuRenderClassTest.php
+++ tests/TestCase/MenuBuilder/BaseMenuRenderClassTest.php
@@ -6,32 +6,65 @@ use Cake\View\View;
 use Menu\MenuBuilder\BaseMenuRenderClass;
 use Menu\MenuBuilder\Menu;
 use Menu\MenuBuilder\MenuItemButton;
+use Menu\MenuBuilder\MenuItemLinkButtonModal;
 
 class BaseMenuRenderClassTest extends TestCase
 {
-    public $view;
-    public $instance;
-
-    public function testConstruct()
+    public function setUp()
     {
-        $menu = new Menu();
+        parent::setUp();
+
         $this->view = new View();
+        $this->menu = new Menu();
 
-        $button = new MenuItemButton();
-        $button->setUrl('http://example.com');
-        $button->setLabel('Test');
-
-        $menu->addMenuItem($button);
-        $this->instance = new BaseMenuRenderClass($menu, $this->view);
-
-        $this->instance->setFormat([
+        $this->menuRenderer = new BaseMenuRenderClass($this->menu, $this->view);
+        $this->menuRenderer->setFormat([
             'menuStart' => '<ul>',
             'menuEnd' => '</ul>',
             'itemStart' => '<li>',
-            'itemEnd' => '</li>',
+            'itemEnd' => '</li>'
         ]);
+    }
 
-        $result = $this->instance->render();
-        $this->assertNotEmpty($result);
+    public function tearDown()
+    {
+        unset($this->menuRenderer);
+        unset($this->menu);
+        unset($this->view);
+
+        parent::tearDown();
+    }
+
+    public function testRenderEmptyMenu()
+    {
+        $expected = '<ul></ul>';
+
+        $this->assertEquals($expected, $this->menuRenderer->render());
+    }
+
+    public function testRenderMenuWithButton()
+    {
+        $item = new MenuItemButton();
+        $item->setUrl('http://example.com');
+        $item->setLabel('Test');
+
+        $this->menu->addMenuItem($item);
+
+        $expected = '<ul><li><button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Test</button></ul>';
+
+        $this->assertEquals($expected, $this->menuRenderer->render());
+    }
+
+    public function testRenderMenuWithModal()
+    {
+        $item = new MenuItemLinkButtonModal();
+        $item->setUrl('http://example.com');
+        $item->setLabel('Modal');
+
+        $this->menu->addMenuItem($item);
+
+        $expected = '<ul><li><a href="#" class="btn btn-default" data-toggle="modal" data-target="#" title="Modal" target="_self"><i class="menu-icon fa fa-"></i> Modal</a></ul>';
+
+        $this->assertEquals($expected, $this->menuRenderer->render());
     }
 }
