fd3fc6372a9c660a5d5cb2563bef15a238d409f5
Leonid Mamchenkov <leonid@mamchenkov.net>
2018-10-30 19:30:26 +0200

    Fixed deprecated references in docblocks (task #8244)

--- src/Controller/MenuItemsController.php
+++ src/Controller/MenuItemsController.php
@@ -39,7 +39,7 @@ class MenuItemsController extends AppController
      * Add method
      *
      * @param string $menuId Menu id
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
+     * @return \Cake\Http\Response|void Redirects on successful add, renders view otherwise.
      */
     public function add($menuId)
     {
@@ -70,8 +70,8 @@ class MenuItemsController extends AppController
      * Edit method
      *
      * @param string|null $id Menu Item id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     * @return \Cake\Http\Response|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Http\Exception\NotFoundException When record not found.
      */
     public function edit($id = null)
     {
@@ -101,7 +101,7 @@ class MenuItemsController extends AppController
      * Delete method
      *
      * @param string|null $id Menu Item id.
-     * @return \Cake\Network\Response|null Redirects to index.
+     * @return \Cake\Http\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function delete($id = null)
@@ -123,7 +123,7 @@ class MenuItemsController extends AppController
      * @param  string $id menu id
      * @param  string $action move action
      * @throws InvalidPrimaryKeyException When provided id is invalid.
-     * @return \Cake\Network\Response|null Redirects to index or referer.
+     * @return \Cake\Http\Response|null Redirects to index or referer.
      */
     public function moveNode($id = null, $action = '')
     {
--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -72,7 +72,7 @@ class MenusController extends AppController
     /**
      * Add method
      *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
+     * @return \Cake\Http\Response|void Redirects on successful add, renders view otherwise.
      */
     public function add()
     {
@@ -95,8 +95,8 @@ class MenusController extends AppController
      * Edit method
      *
      * @param string|null $id Menu id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     * @return \Cake\Http\Response|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Http\Exception\NotFoundException When record not found.
      */
     public function edit($id = null)
     {
@@ -121,7 +121,7 @@ class MenusController extends AppController
      * Delete method
      *
      * @param string|null $id Menu id.
-     * @return \Cake\Network\Response|null Redirects to index.
+     * @return \Cake\Http\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function delete($id = null)
